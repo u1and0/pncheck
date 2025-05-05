ce6da05468899a1e3890679a5316364bd45e6c96
u1and0 <e01.ando60@gmail.com>
2025-05-05 18:04:43 +0900

    [test] sheet.Header.read()

--- lib/input/sheet_test.go
+++ lib/input/sheet_test.go
@@ -2,6 +2,8 @@ package input
 
 import (
 	"testing"
+
+	"github.com/xuri/excelize/v2"
 )
 
 func TestNew(t *testing.T) {
@@ -20,5 +22,46 @@ func TestNew(t *testing.T) {
 	if actual.Header != expected.Header {
 		t.Errorf("got %#v, want: %#v", actual.Header, &expected.Header)
 
+	}
+}
+
+// setValidLayout は正常系のExcelレイアウトを設定します。
+func setValidHeader(f *excelize.File) {
+	// --- Header (入力Ⅱ) ---
+	f.SetCellValue(headerSheetName, projectIDCell, " 12345 ")      // D1: 製番(親)
+	f.SetCellValue(headerSheetName, projectEdaCell, "01")          // F1: 製番(枝) - 読み込み対象外
+	f.SetCellValue(headerSheetName, deadlineHCell, "2023/11/30")   // D2: 製番納期
+	f.SetCellValue(headerSheetName, requestDateCell, "2023/10/27") // D4: 要求年月日
+	f.SetCellValue(headerSheetName, projectNameCell, "テストプロジェクト")  // D5: 製番名称
+	f.SetCellValue(headerSheetName, noteCell, "備考欄テスト")            // D6: 備考
+}
+
+func TestHeaderRead(t *testing.T) {
+	testDir := "testdata_sheet_header_read"
+	testFile := createTestExcelFile(t, testDir, "success-001-read-K.xlsx", setValidLayout)
+
+	expected := Sheet{
+		Config: Config{Validatable: true, Sortable: true},
+		Header: Header{
+			OrderType:   購入,
+			ProjectID:   "1234501",                         // D1 + F1
+			ProjectName: "テストプロジェクト",                       // D5
+			RequestDate: "2023/10/27",                      // D4
+			Deadline:    "2023/11/30",                      // D2
+			FileName:    "pncheck_success-001-read-K.xlsx", // ファイル名 ダミーのpncheck_ prefixがつく
+			Note:        "備考欄テスト",                          // D6
+		},
+	}
+
+	f, err := excelize.OpenFile(testFile)
+	if err != nil {
+		t.Errorf("テスト用Excelファイルが開けません\n")
+	}
+	defer f.Close()
+
+	actual := *New(testFile)
+	actual.Header.read(f)
+	if expected.Header != actual.Header {
+		t.Errorf("got %#v, want: %#v", actual.Header, &expected.Header)
 	}
 }
